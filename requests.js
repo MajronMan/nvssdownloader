2ba2ea4e1d6a0a90f9867132aef698a70739f934
Miron Markowski <majron15@gmail.com>
2020-12-08 11:11:31 +0100

    Remove unnecessary headers

--- requests.js
+++ requests.js
@@ -16,20 +16,6 @@ const mkBody = ({
 }) =>
   `Equinox=${equinox}&PolType=${polarization}&ObjName=&RA=${cra}&Dec=%2B${dc}&Size=${imageSize}&Cells=${pixelSpacing}&MAPROJ=${projection}&rotate=${rotation}&Type=${imageType.header}`;
 
-headers = {
-  accept:
-    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-  "accept-language": "en-GB,en-US;q=0.9,en;q=0.8,pl;q=0.7",
-  "cache-control": "max-age=0",
-  "content-type": "application/x-www-form-urlencoded",
-  "sec-fetch-dest": "document",
-  "sec-fetch-mode": "navigate",
-  "sec-fetch-site": "same-origin",
-  "sec-fetch-user": "?1",
-  "sec-gpc": "1",
-  "upgrade-insecure-requests": "1",
-};
-
 const mkHeaders = (body) => ({
   Accept:
     "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
